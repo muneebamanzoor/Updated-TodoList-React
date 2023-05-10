e1ab30f4b71af37036ddcebde4a1cfe9295895d3
Muneeba Manzoor <61382726+muneebamanzoor@users.noreply.github.com>
2023-05-11 01:28:13 +0500

    commit

--- src/Listcomp.jsx
+++ src/Listcomp.jsx
@@ -21,3 +21,30 @@ const Listcomp =(props)=>{
 } 
 
 export default Listcomp;
+
+
+// import React, { useState } from "react";
+// import ReactDOM from "react-dom";
+// import DeleteIcon from '@mui/icons-material/Delete';
+// import './index.css';
+
+// const Listcomp =(props)=>{
+//     const[line, cutline] = useState(false);
+
+//     const cutIt =()=>{
+//         cutline(true);
+//     }
+    
+//     return(
+//         <>
+//             <div className="todo_style">
+//             <span onClick={cutIt}><DeleteIcon className="deleteIcon"/></span>
+//             <li style = {{textDecoration: line?"line-through":"none"}}>{props.text}</li>
+//             </div>
+//         </>
+//     );
+// } 
+
+// export default Listcomp;
+
+
